f8a1b61f774bcee4894d3126def1703c6c399a06
quanganh.ht97@gmail.com <quanganh.ht97@gmail.com>
2026-06-03 13:18:01 -0700

    AH - Update styling and layout

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
-(function () {
-  [...document.querySelectorAll(".control")].forEach((button) => {
-    button.addEventListener("click", function () {
-      document.querySelector(".active-btn").classList.remove("active-btn");
-      this.classList.add("active-btn");
-      document.querySelector(".active").classList.remove("active");
-      document.getElementById(button.dataset.id).classList.add("active");
-    });
+// (function () {
+//   [...document.querySelectorAll(".control")].forEach((button) => {
+//     button.addEventListener("click", function () {
+//       document.querySelector(".active-btn").classList.remove("active-btn");
+//       this.classList.add("active-btn");
+//       document.querySelector(".active").classList.remove("active");
+//       document.getElementById(button.dataset.id).classList.add("active");
+//     });
+//   });
+// })();
+
+// function submitEmail() {
+//   document.getElementById("contact-form").submit();
+// }
+
+const sections = document.querySelectorAll("section");
+const navItems = document.querySelectorAll("nav .nav-links li");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    if (window.scrollY >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
   });
-  // document.querySelector(".theme-btn").addEventListener("click", () => {
-  //   document.body.classList.toggle("light-mode");
-  // });
 
+  navItems.forEach((li) => {
+    li.classList.remove("active");
 
-})();
-
-function submitEmail() { 
-  document.getElementById("contact-form").submit(); 
-} 
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+});
