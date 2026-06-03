2d82616a66884593e74efa4e331ac7ae647497e4
quanganh.ht97@gmail.com <quanganh.ht97@gmail.com>
2026-06-03 07:47:31 -0700

    AH - Polish layout and styling

--- app.js
+++ app.js
@@ -3,13 +3,17 @@
     button.addEventListener("click", function () {
       document.querySelector(".active-btn").classList.remove("active-btn");
       this.classList.add("active-btn");
-
       document.querySelector(".active").classList.remove("active");
       document.getElementById(button.dataset.id).classList.add("active");
     });
   });
+  // document.querySelector(".theme-btn").addEventListener("click", () => {
+  //   document.body.classList.toggle("light-mode");
+  // });
+
+
 })();
 
-function submitEmail() {
-  document.getElementById("contact-form").submit();
-}
+function submitEmail() { 
+  document.getElementById("contact-form").submit(); 
+} 
